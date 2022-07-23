83b247f209c87fcac62e1f90ca3d3b1648d89239
aya6266 <kylealbert321@gmail.com>
2022-07-23 13:22:18 +0100

    Updates

---
+++ precache-manifest.f8bae6e18ea9b78b41f5d4554401ed72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fe578c54a9f8dcc387086eb91cf5d1",
+    "url": "/My-Art-Your-Art/index.html"
+  },
+  {
+    "revision": "9aac1543bc486af2331d",
+    "url": "/My-Art-Your-Art/static/css/main.95f03438.chunk.css"
+  },
+  {
+    "revision": "21444762900a809791a3",
+    "url": "/My-Art-Your-Art/static/js/2.9b932841.chunk.js"
+  },
+  {
+    "revision": "46c40617de518e60aab602a941eae22b",
+    "url": "/My-Art-Your-Art/static/js/2.9b932841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aac1543bc486af2331d",
+    "url": "/My-Art-Your-Art/static/js/main.cfa66635.chunk.js"
+  },
+  {
+    "revision": "128d686439dd96e15f18",
+    "url": "/My-Art-Your-Art/static/js/runtime-main.bf0de2c0.js"
+  }
+]);
